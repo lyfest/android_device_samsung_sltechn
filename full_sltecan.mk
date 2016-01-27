450294c0d763d3da6a8bd0d37885f30d0cedb6f2
lyfest <1224614551@qq.com>
2016-01-27 15:17:10 +0800

    Update full_sltecan.mk

--- full_sltecan.mk
+++ full_sltecan.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/sltechn/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_sltechn
+PRODUCT_NAME := sltetdzc
 PRODUCT_DEVICE := sltechn
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
