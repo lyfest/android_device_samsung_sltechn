7514f8a85cecbd565f48d6142e7ba8cea0c06476
lyfest <1224614551@qq.com>
2016-01-27 15:22:47 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/sltecan/full_sltecan.mk)
+$(call inherit-product, device/samsung/sltechn/sltetdzc.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -6,13 +6,13 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_sltecan
-PRODUCT_DEVICE := sltecan
+PRODUCT_NAME := cm_sltechn
+PRODUCT_DEVICE := sltechn
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=sltecan \
-    PRODUCT_DEVICE=sltecan \
-    TARGET_DEVICE=sltecan \
-    PRODUCT_MODEL=SM-G850W \
-    BUILD_FINGERPRINT=samsung/sltecan/sltecan:5.0.2/LRX22G/G850WVLU1BOD7:user/release-keys \
-    PRIVATE_BUILD_DESC="sltecan-user 5.0.2 LRX22G G850WVLU1BOD7 release-keys"
+    PRODUCT_NAME=sltetdzc \
+    PRODUCT_DEVICE=sltechn \
+    TARGET_DEVICE=sltechn \
+    PRODUCT_MODEL=SM-G8508S \
+    BUILD_FINGERPRINT=samsung/sltetdzc/sltechn:4.4.4/KTU84P/G8508SZCU1BOI1:user/release-keys \
+    PRIVATE_BUILD_DESC="sltetdzc-user 4.4.4 KTU84P G8508SZCU1BOI1 release-keys"
